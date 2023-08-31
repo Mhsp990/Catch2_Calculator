ed7344d22fafa68de20bd4ba92c1dfbe08a1a29c
Matheus Passos <souza.passos990@gmail.com>
2023-08-31 14:30:21 -0300

    teste

---
+++ test_calculator.cpp
@@ -0,0 +1,27 @@
+// tests/test_calculator.cpp
+#include <catch.hpp>
+#include "calculator.hpp"
+
+TEST_CASE("Calculator Operations", "[calculator]") {
+    SECTION("Addition") {
+        REQUIRE(Calculator::add(2, 3) == 5);
+        REQUIRE(Calculator::add(-2, 2) == 0);
+        REQUIRE(Calculator::add(0, 0) == 0);
+    }
+
+    SECTION("Subtraction") {
+        REQUIRE(Calculator::subtract(5, 3) == 2);
+        REQUIRE(Calculator::subtract(0, 0) == 0);
+    }
+
+    SECTION("Multiplication") {
+        REQUIRE(Calculator::multiply(2, 3) == 6);
+        REQUIRE(Calculator::multiply(4, 0) == 0);
+    }
+
+    SECTION("Division") {
+        REQUIRE(Calculator::divide(6, 3) == 2);
+        REQUIRE(Calculator::divide(5, 2) == Approx(2.5));
+        REQUIRE_THROWS_AS(Calculator::divide(4, 0), std::invalid_argument);
+    }
+}
