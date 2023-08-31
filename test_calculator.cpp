b3c59dbe082a4e0994b474d3bc856dc0ff90f4b4
Matheus Passos <souza.passos990@gmail.com>
2023-08-31 16:23:42 -0300

    mudancas

--- test_calculator.cpp
+++ test_calculator.cpp
@@ -2,7 +2,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "calculator.cpp"
-
+//Test
 TEST_CASE("Calculator Operations", "[calculator]") {
     SECTION("Addition") {
         REQUIRE(Calculator::add(2, 3) == 5);
